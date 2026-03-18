5296ffafb4cb4cd55a210bf7d6f2f03904e0b5f0
Andrew <andrewmail404@gmail.com>
2026-03-17 20:35:52 -0700

    multiple file support

--- cats.asm
+++ cats.asm
@@ -2,14 +2,15 @@ format ELF64 executable 3
 entry _start
 
 segment executable readable
-_start:
+
+printfile:
 	mov eax, 2
-	mov rdi, [rsp + 16]
 	xor esi, esi
 	syscall
 
 	test ax, ax
-	js short _start.open_err
+	js short printfile.open_err
+	push rbx
 	mov ebx, eax
 
 	.printloop:
@@ -21,17 +22,42 @@ _start:
 		syscall
 
 		test ax, ax
-		jg short _start.printloop
+		jg short printfile.printloop
 
 	mov eax, 3
 	mov edi, ebx
 	syscall
 
+	pop rbx
+	ret
+
+	.open_err:
+		pop rbx
+		mov eax, -1
+		ret
+
+_start:
+	mov r12d, [rsp]
+	shl r12d, 3
+
+	mov ebx, 16
+
+	.printargs:
+		mov rdi, [rsp + rbx]
+		call printfile
+
+		test ax, ax
+		js _start.printerr
+
+		add ebx, 8
+		cmp ebx, r12d
+		jle short _start.printargs
+
 	mov eax, 60
 	xor edi, edi
 	syscall
 
-	.open_err:
+	.printerr:
 		mov eax, 60
 		mov edi, 1
 		syscall
